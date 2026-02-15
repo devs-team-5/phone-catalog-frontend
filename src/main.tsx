cc2a9ca6e5827b933e54555738be49cb5e14d275
Dmytro Krylov <dimononk@gmail.com>
2026-02-15 19:54:47 +0200

    chore(SETUP-006): add shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter as Router } from 'react-router';
 import './styles/main.scss';
+import './styles/tailwind.css';
 import App from './modules/Core/App.tsx';
 
 createRoot(document.getElementById('root')!).render(
--- src/modules/Core/App.tsx
+++ src/modules/Core/App.tsx
@@ -1,10 +1,30 @@
 import { Route, Routes } from 'react-router';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 
 function App() {
   return (
     <>
       <h1>Team 5 project</h1>
       <h2>All css reset by _reset.scss</h2>
+      <Select>
+        <SelectTrigger className="w-[300px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectItem value="light">1</SelectItem>
+            <SelectItem value="dark">2</SelectItem>
+            <SelectItem value="system">3</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
       <main>
         <Routes>
           <Route path="/" />
