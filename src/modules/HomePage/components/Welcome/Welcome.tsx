f3fb7b44cc59add0cbfb28a0274ed1dd964f549c
Dmytro Krylov <dimononk@gmail.com>
2026-02-17 20:27:21 +0200

    feat(PC-19): shop by section

--- src/modules/HomePage/components/Welcome/Welcome.tsx
+++ src/modules/HomePage/components/Welcome/Welcome.tsx
@@ -1,10 +1,16 @@
+import { Typography } from '@/components/ui/Typography/Typography';
 import styles from './Welcome.module.scss';
 import { Slider } from './components/Slider';
 
 export const Welcome = () => {
   return (
     <section className={styles.welcome}>
-      <h1 className={styles.title}>Welcome to Nice Gadgets store!</h1>
+      <Typography
+        variant="h1"
+        className={styles.title}
+      >
+        Welcome to Nice Gadgets store!
+      </Typography>
 
       <Slider />
     </section>
