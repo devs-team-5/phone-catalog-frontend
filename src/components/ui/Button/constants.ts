d1229e534e1b809f52e9705e555ba3f73ef8c121
Illia <illiatryndiuk001@gmail.com>
2026-02-21 10:54:58 +0200

    feat(PC-112): add new colors

--- src/components/ui/Button/constants.ts
+++ src/components/ui/Button/constants.ts
@@ -25,6 +25,8 @@ export const BUTTON_COLORS = {
   GREEN: '27AE60',
   RED: 'EB5757',
   // Кольори для вибору товару
+  CLOUDWHITE: 'f8f9f9',
+  LIGHTGOLD: 'ede8e0',
   NATURALTITANIUM: 'a9a49e',
   BLACKTITANIUM: '3d3d3d',
   BLUETITANIUM: '494e53',
@@ -45,7 +47,7 @@ export const BUTTON_COLORS = {
   ROSEGOLD: 'B76E79',
   CORAL: 'FF6F61',
   STARLIGHT: 'F4EAD5',
-  SKYBLUE: '#A7C7E7',
+  SKYBLUE: 'A7C7E7',
 } as const;
 
 export type ButtonSize = keyof typeof BUTTON_SIZES;
